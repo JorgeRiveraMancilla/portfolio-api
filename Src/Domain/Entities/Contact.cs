bf2eb21ab584f7adda70c3f610d2d50b6cd34587
Jorge Rivera Mancilla <jiriveramancilla@gmail.com>
2025-08-02 17:17:43 -0400

    feat: add contact entity

---
+++ Src/Domain/Entities/Contact.cs
@@ -0,0 +1,13 @@
+namespace Domain.Entities
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public string IpAddress { get; set; } = string.Empty;
+    }
+}
